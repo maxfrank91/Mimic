9a6ecfa0e3bd98e7f33f1d426385295ec9d72a7d
Maximilian Frank <Maximilian.F1@gmx.de>
2014-11-24 10:40:48 +0100

    create

---
+++ Assets/Crackle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crackle : MonoBehaviour 
+{
+    public GameObject crackle;
+    Vector3 center;
+    float range = 0.5f;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        center = transform.position;
+	}
+}
---
+++ Assets/Trail.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trail : MonoBehaviour 
+{
+    Vector3 lastPos, actualPos;
+
+	// Use this for initialization
+	void Awake () 
+    {
+        lastPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        actualPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        actualPos.z += 10;
+        transform.position = actualPos;
+
+        if (lastPos != actualPos)
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, transform.rotation.eulerAngles.z + (Vector3.Angle(lastPos, actualPos))));
+        lastPos = actualPos;
+	}
+}
